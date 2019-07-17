aa3f789eff2dd55757665fcb5f63a32de439e1ca
Oscar Olivares <oscarolivares625@gmail.com>
2019-07-16 21:19:39 -0400

    Se instalo un middleware parecido al personalizado <morgan>

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
 const express = require('express');
+const morgan = require('morgan')
+
 const app = express();
+
 
 // OJO los middleware se usan antes de definir las rutas
 
-// Creando un middleware
+// Creando un middleware personalizado
 function logger(req, res, next) {
   console.log('Middleware executed');
   console.log(`TargetUrl: ${req.protocol}://${req.get('host')}${req.originalUrl}`);
   next();
 }
 
+// Instanciación del middelware personalizado
+app.use(logger);
+
 // Para que express entienda las peticiones json ejem en: res.body de /user |POST se utiliza el siguiente middleware
 app.use(express.json());
-// Instanciación del middelware
-app.use(logger);
+
+// Middleware de morgan para hacer un log de la peticion = a al md personalizado
+app.use(morgan('dev'));
+
+
+
 
 
 app.get('/', (req, res) => {
@@ -25,10 +35,10 @@ app.get('/', (req, res) => {
   Notar el parámetro "next" se agrega para que tambien ejecute la siguiente coincidencia para /user
   incluyendo "/user |GET", "/user/:id |POST", etc.
 */
-/* app.all('/user', (req, res, next) => {
+app.all('/user', (req, res, next) => {
   console.log('Method all for /user was executed');
   next();
-}) */
+})
 
 app.get('/user', (req, res) => {
   res.json({
